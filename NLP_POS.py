5d8d5b6732a8dc5fae03343860f66742af5d6fc6
Elif YILDIRIM <eliff.celikel@gmail.com>
2020-12-15 19:02:23 +0300

    Update NLP_POS.py

--- NLP_POS.py
+++ NLP_POS.py
@@ -68,6 +68,186 @@ print(pos_tagging_s())
 
 # In[ ]:
 
+##############################
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+from textblob import TextBlob
+import nltk
+
+
+# In[2]:
+
+
+get_ipython().system('pip install TextBlob')
+
+
+# In[4]:
+
+
+mystr = """The best error message is the one that never shows up.
+You Learn More From Failure Than From Success. 
+Don’t Let It Stop You. Failure Builds Character.
+If You Are Working On Something That You Really Care About, You Don’t Have To Be Pushed. The Vision Pulls You.
+The purpose of software engineering is to control complexity, not to create it"""
+
+
+# In[5]:
+
+
+blob = TextBlob(mystr)
+
+
+# In[6]:
+
+
+type(blob)
+
+
+# In[7]:
+
+
+from textblob.blob import BaseBlob
+
+
+# In[8]:
+
+
+base_blob = BaseBlob(mystr)
+
+
+# In[9]:
+
+
+type(base_blob)
+
+
+# Word Tokenizing
+# 
+
+# In[10]:
+
+
+blob
+
+
+# In[14]:
+
+
+nltk.download('punkt')
+
+
+# In[15]:
+
+
+blob.words
+
+
+# Sentence Tokenizing
+# 
+
+# In[16]:
+
+
+blob.sentences
+
+
+# In[17]:
+
+
+
+# Words tokens
+for word_tokens in blob.sentences:
+    print(word_tokens.words)
+
+
+# N-Grams
+# 
+
+# In[18]:
+
+
+
+# Bi-Gram
+for bigram in blob.ngrams(2):
+    print(bigram)
+
+
+# In[19]:
+
+
+# Tri-Gram
+for trigram in blob.ngrams(3):
+   print(trigram)
+
+
+# Part of Speech
+# 
+
+# In[20]:
+
+
+mystr
+
+
+# In[21]:
+
+
+blob1 = TextBlob("Hello world this is NLP with TextBlob for text classification.")
+
+
+# In[24]:
+
+
+blob1.tags
+
+
+# In[23]:
+
+
+get_ipython().system('python -m textblob.download_corpora')
+
+
+# In[25]:
+
+
+## Alternative Part of Speech
+blob1.pos_tags
+
+
+# Noun_Phrases
+
+# In[26]:
+
+
+blob2 = TextBlob("Google is great search engine for finding almost anything ")
+
+
+# In[27]:
+
+
+
+for np in blob2.noun_phrases:
+    print(np)
+
+
+# In[28]:
+
+
+# ['VB','VBZ','VBP','VBD','VBN','VBG']
+for word,verb in blob.tags:
+    if verb in ['VB','VBZ','VBP','VBD','VBN','VBG']:
+        print(f'{word} => {verb}')
+
+
+# In[ ]:
+
+
+
+
 
 
 
